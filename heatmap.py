848ed06c50cc6f27c2e835c440e5193f41bfec86
Simon Meier-Vieracker <39825770+fussballlinguist@users.noreply.github.com>
2025-01-17 09:12:05 +0100

    Update heatmap.py

--- heatmap.py
+++ heatmap.py
@@ -8,7 +8,9 @@ df = pd.read_csv("btw25.tsv", sep="\t", quoting=3)
 
 # Dropdown for part-of-speech selection
 pos_options = ["NN", "ADJ", "VV"]
-selected_pos = st.selectbox("Select Part-of-Speech", pos_options)
+selected_pos = st.selectbox("Wähle die Wortart aus:", pos_options)
+
+st.write("Legende: NN = Substantiv, ADJ = Adjektiv, VV = Verb")
 
 # Filter data based on the selected POS
 df_filtered = df[df.pos.str.contains(selected_pos)]
@@ -25,11 +27,11 @@ matrix = top_df.pivot_table(index='lemma', columns='party', values='relative_fre
 matrix = matrix.fillna(0)
 
 # Plot heatmap
-st.write("### Heatmap of Relative Frequencies")
+st.write("### Wahlprogramm-Heatmap")
 plt.figure(figsize=(3, 6))
 sns.set_theme(style="whitegrid")
 chart = sns.heatmap(data=matrix, annot=False, cmap="Reds")
 st.pyplot(plt)
 
-st.write("The heatmap above shows the relative frequency of the top 25 lemmas per party.")
-st.write("Use the dropdown menu to change the part-of-speech (POS) and explore different distributions.")
+st.write("Die Heatmap zeigt die relativen Häufigkeiten für die 25 häufigsten Substantive je nach Partei.\nGerechnet wurde auf lemmatisierter Basis.\nDatengrundlage sind die Wahlprogramme zur Bundestagswahl 2025.")
+st.write("Entwickelt von [Simon Meier-Vieracker](https://tu-dresden.de/gsw/slk/germanistik/al/die-professur/inhaber), Januar 2025.")
